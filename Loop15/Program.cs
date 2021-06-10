cc07d1e20f77b689a8b3224dd70648882ae64a88
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-06-10 04:37:30 -0600

    Add project files.

---
+++ Loop15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Loop15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//loop 15
+            var i = 0;
+
+            while (i < 15)
+            {
+                Console.WriteLine(i);
+                i++;
+
+            }
+            //will only loop by even numbers
+            var d = 2;
+            while (d < 28)
+            {
+                if(d % 2 == 0)
+                {
+                    Console.WriteLine(d);
+                }
+                d++;
+            }
+
+            
+        }
+    }
+}
